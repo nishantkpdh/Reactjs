1ef68f3cb401ac0e0c35b9a9a962961910cae77f
nishant_kumar <nishantkumar@nousinfo.com>
2020-01-23 17:01:25 +0530

    Updates

---
+++ precache-manifest.562cb05cee555e1f04d91361bdfda7a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded09e08d904f287c8ed07172c9855f0",
+    "url": "/Reactjs/index.html"
+  },
+  {
+    "revision": "4f96a95b9a2415fd3518",
+    "url": "/Reactjs/static/css/main.416becef.chunk.css"
+  },
+  {
+    "revision": "08a06e965161c57dc51a",
+    "url": "/Reactjs/static/js/2.5d89cc32.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Reactjs/static/js/2.5d89cc32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f96a95b9a2415fd3518",
+    "url": "/Reactjs/static/js/main.d5118726.chunk.js"
+  },
+  {
+    "revision": "5eeaf3327fefbd6ca02c",
+    "url": "/Reactjs/static/js/runtime-main.69dbb1e2.js"
+  }
+]);
